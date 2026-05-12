c02b8f888b1ff72dda5029048352c703f6fd908d
Leopoldo-Medeiros <leopoldof.medeiros@gmail.com>
2026-05-12 18:25:40 +0100

    Fix React import and FC type declaration

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { SPECIALTIES } from '../constants';
 import { User, Award } from 'lucide-react';
 
-const About: React.FC = () => {
+const About: FC = () => {
   return (
     <>
       {/* Specialties Section */}
